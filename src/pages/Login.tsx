6a96a1585d50a9ab802e95dc6ae31a282d5f8244
Cesar Brandon <cesarbrandon000@gmail.com>
2023-01-24 00:38:19 -0500

    fix:🐛add images

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Layout } from "../components/common";
 import FormLogin from "../components/layouts/FormLogin";
+import ImageLoginIfv from "../assets/image-login-ifv.jpg";
 
 const Login: React.FC = () => {
   return (
@@ -11,10 +12,7 @@ const Login: React.FC = () => {
             <FormLogin />
           </div>
           <div className="Login__content_image">
-            <img
-              src="http://www.gllg.edu.pe/img/ifv/1.jpg"
-              alt="instituto-image"
-            />
+            <img src={ImageLoginIfv} alt="instituto-image" />
           </div>
         </div>
       </div>
